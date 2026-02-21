1aa81d5d1e88618b89d82155d1a77758da4a884b
Paarth Agrawal <agrawal.a.paarth@gmail.com>
2026-02-21 14:25:36 -0600

    Updated fuel subsystem

--- src/main/java/frc/robot/subsystems/CANFuelSubsystem.java
+++ src/main/java/frc/robot/subsystems/CANFuelSubsystem.java
@@ -19,7 +19,6 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
-import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
