00011331b18249e0b69937a693aa10135112d40e
Vivaan Agrawal <agrawal.vivaan@gmail.com>
2026-02-21 16:17:49 -0600

    Added the CTRE CANBus import

--- src/main/java/frc/robot/subsystems/CANFuelSubsystem.java
+++ src/main/java/frc/robot/subsystems/CANFuelSubsystem.java
@@ -4,6 +4,8 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.CANBus;
+
 // SparkMax imports (commented out for now since we're using TalonFX instead)
 //import com.revrobotics.spark.SparkBase.PersistMode;
 //import com.revrobotics.spark.SparkBase.ResetMode;
@@ -17,6 +19,7 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.CANBus;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
